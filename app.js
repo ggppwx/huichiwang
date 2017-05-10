c7b9d4737a8c9c580749b0e698139de1e77a83b7
roy gu <sirgujingwei@gmail.com>
2017-05-09 22:35:31 -0400

    first

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var expressSession = require('express-session');
+var flash = require('connect-flash');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var shop = require('./routes/shop');
+var login = require('./routes/login');
+var signup = require('./routes/signup');
+var auth = require('./routes/api/auth');
+var app = express();
+
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connect("mongodb://localhost:27017/Huichi"); 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/shop', shop);
+app.use('/login', login);
+app.use('/signup', signup);
+app.use("/api/auth", auth );
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
