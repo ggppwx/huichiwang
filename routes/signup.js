346a0d4ce581a5a9972bc9bc65f4e9c532904968
roy gu <sirgujingwei@gmail.com>
2017-07-20 20:08:14 -0400

    update

--- routes/signup.js
+++ routes/signup.js
@@ -1,25 +1,39 @@
 var express = require('express');
 var passport = require('passport');
 var User = require('../models/user');
+var svgCaptcha = require('svg-captcha');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('signup', { title: 'Express' });
+  var captcha = svgCaptcha.create();  
+  req.session.captcha = captcha.text;  
+  var validcode = captcha.data;
+  console.log(captcha.text);
+  res.render('signup', { title: 'Express', validcode : validcode });
 });
 
+
+
 router.post('/', function(req, res) {
+
+  // check if the captcha is right 
+
+  if (req.body.captcha.toLowerCase() != req.session.captcha.toLowerCase() ) {
+    console.log('valid code error');
+    return res.redirect("/signup");
+
+  }
+
       User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
           if (err) {
           	console.log('error in register ...');
-            return res.render("signup", {info: "Sorry. That username already exists. Try again."});
+            return res.redirect('/signup');
           }
 
           passport.authenticate('local')(req, res, function () {
           	console.log(res);
-            res.render('/', {
-            	user : 'test'
-            });
+            res.redirect('/');
           });
       });
 });
